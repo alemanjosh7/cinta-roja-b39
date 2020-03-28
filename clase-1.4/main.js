4dca29e96393c114b2b7dddc2a222e6b62b65524
JoshMA503 <josuealeman2003@gmail.com>
2020-03-27 21:37:35 -0600

    Clase 1.4-Async

---
+++ clase-1.4/main.js
@@ -0,0 +1,115 @@
+//const mostrarMensaje = () => console.log("Paso un segundo!");
+//console.log("Funciona");
+//Funcion de orden superior
+/*setTimeout(
+    mostrarMensaje, // Callback
+    50000
+);
+*/
+//setTimeout es un proceso asincrono
+//ES6
+function sumarVanilla(a, b){
+    console.log("Vamos a sumar dos numeros!");
+    return a + b;
+}
+
+const sumarArrow = (a, b) => {
+    console.log("Vamos a sumar dos numeros!");
+    return a + b;
+} 
+// JS Vanilla
+
+const esPar = (numero) => {
+    if(numero % 2 === 0) return true;
+    else return false;
+}
+
+const esParLaSumaDeDosNumeros = (numero1, numero2) => {
+    const res = sumarArrow(numero1, numero2);
+    const otroRes = esPar(res);
+    return otroRes;
+}
+
+// const esPar2 = esPar(1);
+// const esPar3 = esPar(3);
+// if(esPar2 == esPar3) console.log("Los dos numeros son pares");
+// codigo sincrono 
+// codigo asincrono es cuando js deteca que cuando es un codigo asincrono lo manda a otra seccion a ejecutarse
+
+// console.log(esParLaSumaDeDosNumeros(5, 7));
+//Element.addEventLister("click", callback);
+
+// const mensajeEnConsola = (callback, mensaje) => {
+//     for(let i = 0; i < 5; i++){
+//         callback(mensaje);
+//     }
+// }
+//mensajeEnConsola(() => {console.log("Hola")});
+// mensajeEnConsola(console.log, "Hola");
+// const highOrder = (variable, callback) => {
+//     callback(variable);
+// }
+// const getDatatype = (variable) => typeof(variable);
+
+// const contentCallBack = (variable) => {
+//     console.log(getDatatype(variable));
+//     console.log(variable);
+// }
+// const miVar = {nombre: "Josue"};
+// highOrder(num, contentCallBack);
+
+//1
+// const sumaDeDosNumeros = (num1, num2, callback) => {
+//     return callback(num1, num2);
+// }
+// const resta = (num1, num2) => num1 - num2;
+// const suma = (num1, num2) => num1 + num2;
+// const multi = (num1, num2) => num1 * num2;
+
+// const contentCallBack = (num1, num2) => {
+//     console.log(resta(num1, num2));
+//     console.log(suma(num1, num2));
+//     console.log(multi(num1, num2));
+// }
+
+// const res1 = sumaDeDosNumeros(2,2, resta);
+// const res2 = sumaDeDosNumeros(2,2, suma);
+// const res3 = sumaDeDosNumeros(2,2, multi);
+// console.log(res1);
+// console.log(res3);
+// console.log(res2);
+
+
+//2
+// const convertidorPalabras = (palabra, callback) => {
+//     callback(palabra);
+// }
+
+// const minus = (palabra) => palabra.toLowerCase();
+// const upper = (palabra) => palabra.toUpperCase();
+
+// const contentCallBack = (palabra) => {
+//     console.log(minus(palabra));
+//     console.log(upper(palabra));
+// }
+
+// convertidorPalabras("Hola", contentCallBack);
+
+//3
+
+const mayorADosHoras = (arr, callback) => {
+    callback(arr);
+}
+const res = (arr) => {
+    for(let i = 0; i < 4; i++){
+        if(arr[i] > 120){
+            console.log(arr[i]);
+        }
+    }
+}
+
+mayorADosHoras([150, 130, 10, 1], res);
+
+
+
+
