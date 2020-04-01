521e29a78b47e4ff0e7d375a89b84bb71886fe34
JoshMA503 <josuealeman2003@gmail.com>
2020-03-31 22:34:13 -0600

    Using the NASA, POKEMON, STARWARS APIS

--- clase-2.1/client.js
+++ clase-2.1/client.js
@@ -11,10 +11,123 @@ const request = require('request');
 // });
 // console.log("Ha terminado la peticion...");
 
-const SWAPI_URL = "https://swapi.co/api/people/1";
+const SWAPI_URL = "https://pokeapi.co/api/v2/pokemon/1"; //endpoint
+// request.get(SWAPI_URL, (err, res, body) => {
+//     console.log(res.statusCode);
+//     const json = JSON.parse(body);
+//     console.log(json);
+// })
+const POKEAPI_URL = "https://pokeapi.co/api/v2/pokemon/1";
+const OPENLIBRARY_URL = "http://openlibrary.org/search.json?q=i+robot";
+const AUTHOR_URL = "http://openlibrary.org/search.json?author=asimov";
 
-request.get(SWAPI_URL, (err, res, body) => {
-    console.log(res.statusCode);
-    const json = JSON.parse(body);
-    console.log(`Mi nombre es ${json.name} y naci en el ${json.birth_year}`);
-})
+//1
+
+// request.get(POKEAPI_URL, (err, res, body) => {
+//     if(res.statusCode === 200){
+//         const json = JSON.parse(body);
+//         console.log(`${json.name} has the next types:`)
+//         json.types.forEach(type => {
+//             console.log(type.type.name);
+//         });
+//     } else{
+//         console.log(res.statusCode);
+//     }
+// });
+
+    //2
+// request.get(OPENLIBRARY_URL, (err, res, body) => {
+//     if(res.statusCode === 200){
+//         const json = JSON.parse(body);
+//         const doc = json.docs[0];
+//             console.log(`${doc.title_suggest} is the name of the book`);
+//             if(doc.author_name){
+//                 console.log(`${doc.author_name[0]} is the author`);
+//             }
+//     } else{
+//         console.log(res.statusCode);
+//     }
+// });
+
+    //3
+
+// request.get(AUTHOR_URL, (err, res, body) => {
+//     if(res.statusCode === 200){
+//         const json = JSON.parse(body);
+//         const doc = json.docs[0];
+//         if(doc.author_name[0]){
+//             console.log(`The author is ${doc.author_name[0]}`);
+//         }
+//         json.docs.forEach(doc => {
+//             if(doc.author_name[0] == doc.author_name){
+//                 console.log(doc.title_suggest);
+//             }
+//         });
+//     } else{
+//         console.log(res.statusCode);
+//     }
+// });
+
+    // 4
+
+// const AUDIO_URL = " http://www.theaudiodb.com/api/v1/json/1/search.php?s=muse";
+// request.get(AUDIO_URL, (err, res, body) => {
+//     if(res.statusCode === 200){
+//         const json = JSON.parse(body);
+//         const band = json.artists;
+//             console.log(`${band[0].strArtist} is the name of the band`);
+//             if(band[0].strStyle){
+//                 console.log(`${band[0].strGenre} is the genre`);
+//             }
+//     } else{
+//         console.log(res.statusCode);
+//     }
+// });
+
+    //5
+
+const NAME_URL = "https://swapi.co/api/people/13/"
+//         json.docs.forEach(doc => {
+//             if(doc.author_name[0] == doc.author_name){
+//                 console.log(doc.title_suggest);
+//             }
+//         });
+request.get(NAME_URL, (err, res, body) => {
+    if(res.statusCode === 200){
+        const json = JSON.parse(body);
+            console.log(`The author is ${json.name}`);
+            json.films.forEach((film) =>{
+                request.get(film, (err, res, body) => {
+                    const jsonFilmes = JSON.parse(body);
+                    console.log(jsonFilmes.title);
+                })
+            })
+    } else{
+        console.log(res.statusCode);
+    }
+});
+//https://api.nasa.gov/neo/rest/v1/feed?start_date=START_DATE&end_date=END_DATE&api_key=tPwgiDUJ0CbmxQraXVvuMjrgZN3WFRw1x3CJZhAr
+    //6
+const START_DATE = '2020-03-23';
+const END_DATE = '2020-03-30';
+const API_KEY = 'tPwgiDUJ0CbmxQraXVvuMjrgZN3WFRw1x3CJZhAr'
+const NASA_URL = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${START_DATE}&end_date=${END_DATE}&api_key=tPwgiDUJ0CbmxQraXVvuMjrgZN3WFRw1x3CJZhAr`;
+request.get(NASA_URL, (err, res, body) => {
+    if (res.statusCode === 200) {
+      const json = JSON.parse(body);
+      // console.log(json.near_earth_objects.2020-03-029);
+      const dates = (Object.values(json.near_earth_objects));
+      dates.forEach((date) => {
+          const meteorite = Object.values(date);
+          meteorite.forEach((name) => {
+              console.log(name.name);
+          })
+      })
+    } 
+  });
+
+const POKEMON_URL = 'https://pokeapi.co/'
+
+
+
+
